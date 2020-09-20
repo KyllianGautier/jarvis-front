258663416bbd9f3fb87836ad843ea926771bad28
Kyllian <kyllian.gt@hotmail.fr>
2020-09-20 13:59:53 +0200

    Setup basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
+import { paths } from './shared/constants/app-paths';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: paths.home, pathMatch: 'full' },
+  { path: paths.home, component: HomeComponent },
+  { path: paths.login, component: LoginComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
